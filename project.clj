5bb58067a16192f51aeda9083678d79924887b7f
Stan Rozenraukh <stan@stanistan.com>
2015-12-26 13:39:49 -0500

    Things are now ignored in the public/js dir

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]]
   :plugins [[lein-figwheel "0.5.0-1"]]
-  :clean-targets [:target-path "out" "resources/public/cljs"]
+  :clean-targets [:target-path "out" "resources/public/js"]
   :cljsbuild { :builds [ {:id "dev"
                           :source-paths ["src"]
                           :figwheel true
                           :compiler {:main color-transit.core
-                                     :asset-path "cljs/out"
-                                     :output-to "resources/public/cljs/main.js"
-                                     :output-dir "resources/public/cljs/out" } }] })
+                                     :asset-path "js/out"
+                                     :output-to "resources/public/js/main.js"
+                                     :output-dir "resources/public/js/out" } }] })
