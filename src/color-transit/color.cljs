f68502ab26d4ad640bf31a2209e96ac4d5291e8a
Stan Rozenraukh <stan@stanistan.com>
2015-12-26 13:13:42 -0500

    working!

--- src/color-transit/color.cljs
+++ src/color-transit/color.cljs
@@ -1,12 +1,41 @@
 (ns color-transit.color)
 
 (defn style 
-  [r g b]
+  [[r g b]]
   (str "rgb(" r "," g "," b ")"))
 
 (defn delta
-  [[r1 g1 b1] [r2 g2 b2]]
-  [(- r2 r1)
-   (- g2 g1)
-   (- b2 b1)])
+  [[r1 g1 b1] [r2 g2 b2] steps]
+  [(/ (- r2 r1) steps)
+   (/ (- g2 g1) steps)
+   (/ (- b2 b1) steps)])
 
+(defn find-next
+  "Given a color and the list of colors to choose from, find the 
+   next color.  
+   
+   Returns the default value if the current color is not found 
+   in the list."
+  [current color-list]
+  (let [reducer (fn [found-enum c] 
+                  (println "case" current found-enum c)
+                  (case found-enum
+                    nil (if (= current c) true nil)
+                    true c
+                    found-enum))]
+    (let [v (reduce reducer nil color-list)]
+      (if (vector? v) 
+        v
+        (first color-list)))))
+
+(defn +delta
+  [[r g b] [r+ g+ b+] scalar]
+  (map Math/floor 
+       [(+ r (* scalar r+))
+        (+ g (* scalar g+))
+        (+ b (* scalar b+))]))
+
+(defn create-queue
+  [from-color to-color steps]
+  (let [d (delta from-color to-color steps)]
+    (conj (map #(+delta from-color d %) (range 0 (inc steps))))))
--- src/color-transit/core.cljs
+++ src/color-transit/core.cljs
@@ -3,7 +3,10 @@
             [color-transit.canvas :refer [ctx id->Canvas fill-style
                                           fill-rect]]))
 
-(defonce app-state (atom {}))
+(defonce app-state (atom {:interval nil 
+                          :colors []
+                          :current-color nil
+                          :color-queue []}))
 
 (defn set-interval
   [f timeout]
@@ -14,24 +17,51 @@
   (when interval 
     (.clearInterval js/window interval)))
 
-(defn swap-interval
+(defn swap-interval!
   [state f timeout]
   (clear-interval (:interval @state))
   (swap! state assoc :interval (set-interval f timeout)))
 
+(defn draw-rect
+  [canvas color]
+  (-> canvas
+      (ctx fill-style color)
+      (ctx fill-rect 0 0 (:w canvas) (:h canvas))))
+
+(defn compute-color-from-map
+  [{:keys [color-queue current-color colors steps] :as color-map}]
+  (let [with-color-queue (fn [c q] (assoc color-map :color-queue q :current-color c))]
+    (if (not (empty? color-queue))
+      (with-color-queue (first color-queue) (rest color-queue))
+      (let [next-color (color/find-next current-color colors)]
+        (recur (with-color-queue
+                 next-color
+                 (color/create-queue current-color next-color steps)))))))
+
+(defn compute-color
+  [state]
+  (let [color-map (compute-color-from-map @state)
+        color (color/style (:current-color color-map))]
+    (swap! state merge color-map)
+    color))
+
 (defn run-loop
   "Executes drawing every whatever time we defined."
   [canvas app-state]
-  (-> canvas
-      (ctx fill-style (color/style 0 0 0))
-      (ctx fill-rect 0 0 (:w canvas) (:h canvas))))
-
-(def fps (/ 1000 60))
+  (let [color (compute-color app-state)] 
+    (draw-rect canvas color)))
 
 (defn run-app!
-  []
-  (swap-interval app-state
+  [colors fps]
+  (swap! app-state assoc :steps 30
+                         :colors colors
+                         :current-color (first colors))
+  (swap-interval! app-state
                  #(run-loop (id->Canvas "myCanvas") app-state)
                  fps)) 
 
-(run-app!)
+(enable-console-print!)
+
+(run-app! 
+  [[0 10 0], [200 155 255], [40 40 40], [255 0 0]]
+  (/ 1000 30))
